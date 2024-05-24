9cc2403640412ed8987a08e058c18476281f6da6
Sterne Lee <xingguang@clare.ai>
2024-05-24 13:15:14 +0800

    feat: support cloudflare pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+// import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 const mode = process.env.NEXT_PUBLIC_BUILD_MODE ?? 'standalone'
+
+// if (process.env.NODE_ENV === 'development') {
+//   await setupDevPlatform();
+// }
 
 const nextConfig = {
   transpilePackages: ['crypto-js'],
