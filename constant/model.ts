543d0bb39b19f88dcb29d313dc37f28e75aa4471
Amery2010 <amery@xiangfa.org>
2026-03-04 22:58:34 +0800

    feat: IP-based initial locale(@kkgs-32) fix: Model list parsing fix and unification(@kkgs-32) fix: Mobile viewport-height handling(@kkgs-32) chore: Update the default model list

--- constant/model.ts
+++ constant/model.ts
@@ -1,18 +1,14 @@
 export const Model: Record<string, string> = {
+  'gemini-flash-latest': 'Gemini Flash Latest',
+  'gemini-3-flash-preview': 'Gemini 3 Flash Preview',
+  'gemini-3.1-pro-preview': 'Gemini 3.1 Pro Preview',
+  'gemini-3.1-flash-lite-preview': 'Gemini 3.1 Flash-Lite Preview',
   'gemini-2.5-flash': 'Gemini 2.5 Flash',
-  'gemini-2.5-pro': "Gemini 2.5 Pro",
-  'gemini-2.0-flash': 'Gemini 2.0 Flash',
-  'gemini-2.0-flash-lite': 'Gemini 2.0 Flash Lite',
-  'gemini-1.5-pro': 'Gemini 1.5 Pro',
-  'gemini-1.5-pro-latest': 'Gemini 1.5 Pro Latest',
-  'gemini-1.5-flash': 'Gemini 1.5 Flash',
-  'gemini-1.5-flash-latest': 'Gemini 1.5 Flash Latest',
-  'gemini-1.5-flash-8b': 'Gemini 1.5 Flash-8B',
-  'gemini-1.5-flash-8b-latest': 'Gemini 1.5 Flash-8B Latest',
+  'gemini-2.5-pro': 'Gemini 2.5 Pro',
 }
 
 export const OldVisionModel = ['gemini-pro-vision', 'gemini-1.0-pro-vision-latest']
 
 export const OldTextModel = ['gemini-1.0-pro', 'gemini-1.0-pro-latest', 'gemini-pro']
 
-export const DefaultModel = 'gemini-2.5-flash'
+export const DefaultModel = 'gemini-flash-latest'
